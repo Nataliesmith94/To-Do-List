60e00713571f33c0b117973cb34d272d6d426a98
Natalie Smith <natalie.smith@moneysupermarket.com>
2018-09-25 08:30:21 +0100

    to do list first commit

---
+++ resources/js/main.js
@@ -0,0 +1,17 @@
+
+function buttonClick() {
+    console.log('Button Clicked');
+}
+
+
+//User Clicked on the add button
+//If there is any text in the item field add that text to the to-do list
+document.getElementById('add').addEventListener('click', function() {
+
+    var value = document.getElementById('item').value;
+
+    if (value) {
+
+    }
+
+});
